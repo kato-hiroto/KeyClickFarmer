da03b5dac8e14fbb90c526c349e111e42da91792
karofuji <kato-hiroto@ed.tmu.ac.jp>
2019-11-28 19:17:01 +0900

    計算クラス修正

--- KeyClickFarmer/src/scripts/decimal.ts
+++ KeyClickFarmer/src/scripts/decimal.ts
@@ -1,56 +1,57 @@
-import { QuickInputButtons } from "vscode";
+// 整数部(DIGIT * INT)桁，小数部(DIGIT)桁の正の数（0含む）が正しく扱える数値型
+export class Decimal {
 
-// 整数部32桁，小数部8桁の正の数（0含む）が正しく扱える数値型
-class Decimal {
+    static readonly DIGIT = 8
+    static readonly INT = 4     // _intValue[1] ~ _intValue[INT]が整数値の保存領域
 
     private _strValue: string = "0";
-    private _intValue: number[] = new Array(5);
+    private _intValue: number[] = new Array(Decimal.INT + 1);
 
     constructor(val : string) {
-        // コンストラクタ
-        this.setValue(val);
+        // 値を代入する
+        this._strValue = val;
+        this.parseInt();
     }
-
-    public getValue() : string {
+    
+    public toString() : string {
         // 値を出力する
         this.parseStr();
         return this._strValue;
     }
 
-    public setValue(val : string) : string {
-        // 値を代入する
-        this._strValue = val;
-        this.parseInt();
-        return val;
+    public toNumbers(): number[] {
+        // 値を出力する
+        return this._intValue;
     }
 
-    private parseInt() {
+    private parseInt(): void {
         const val = this._strValue;
         const dotPos : number = val.indexOf(".");
         // 小数部
-        const tmpStr = val.substring(dotPos + 1);
-        if (dotPos == tmpStr.length - 1 || dotPos < 0) {
+        const decStr = val.substring(dotPos + 1);
+        if (dotPos == decStr.length - 1 || dotPos < 0) {
             // 小数点が文字列のラスト，あるいは小数点がないなら0を代入
             this._intValue[0] = 0;
-        } else if (tmpStr.length <= 8) {
+        } else if (decStr.length <= Decimal.DIGIT) {
             // 8文字以下なら8桁になるよう0埋め
-            this._intValue[0] = Number(tmpStr) * Math.pow(10, 8 - tmpStr.length);
+            this._intValue[0] = Number(decStr) * Math.pow(10, 8 - decStr.length);
         } else {
             // 8文字を超えるなら超えた部分はカット
-            this._intValue[0] = Number(tmpStr.substring(0, 8));
+            this._intValue[0] = Number(decStr.substring(0, 8));
         }
         // 整数部
-        for (let i = 1; i < 5; i++) {
-            const startPos = Math.max(val.length - 8 * (i + 1) - dotPos, 0);
-            const endPos = Math.max(val.length - 8 * i - dotPos, 0);
-            const tmpStr = val.substring(startPos, endPos);
+        const intStr = val.substring(0, dotPos);
+        for (let i = 1; i <= Decimal.INT; i++) {
+            const startPos = Math.max(val.length - Decimal.DIGIT * i, 0);
+            const endPos = Math.max(val.length - Decimal.DIGIT * (i - 1), 0);
+            const tmpStr = intStr.substring(startPos, endPos);
             this._intValue[i] = tmpStr === "" ? 0 : Number(tmpStr);
         }
     }
 
-    private parseStr() {
+    private parseStr(): void {
         // 整数部
-        for (let i = 4; i > 0; i--) {
+        for (let i = Decimal.INT; i > 0; i--) {
             const tmp = this._intValue[i];
             this._strValue += tmp === 0 ? "" : tmp.toString();
         }
@@ -58,74 +59,90 @@ class Decimal {
         const tmp = this._intValue[0];
         this._strValue += tmp === 0 ? "" : "." + tmp.toString();
     }
-
-    private MoveUp(index : number) {
-        // 指定した_intValueについて桁上がりがあれば実行
-        const dig = 100000000;
-        const val = this._intValue[index];
-        const small = val % dig;
-        const big = (val - small) / dig;
-        if (index + 1 < 5) {
-            this._intValue[index + 1] += big;
-        }
-        this._intValue[index] = small;
-    }
     
-    private CarryDown(index : number) {
-        // 指定した_intValueについて繰り下がりがあれば実行
-        const dig = 100000000;
-        const size = Math.max(Math.ceil(-1 * this._intValue[index] / dig), 0);
-        const val = this._intValue[index] + dig * size      // 負数の分を埋め合わせ
-        const small = val % dig
-        const big = size
-        if (index + 1 < 5) {
-            this._intValue[index + 1] -= big;
+    private Carry(index : number): void {
+        // indexで指定した_intValueについて繰り上がり・繰り下がりがあれば実行
+        const dig = Math.pow(10, Decimal.DIGIT);
+        const val = this._intValue[index];
+        if (val >= 0) {
+            // 繰り上がりがあれば実行
+            const small = val % dig;
+            const big = (val - small) / dig;    // 繰り上がり分
+            if (index < Decimal.INT) {
+                // 最上位桁を超過した分はこの処理ができず切り捨てられる
+                this._intValue[index + 1] += big;
+            }
+            this._intValue[index] = small;
+        } else {
+            // 繰り下がりの実行
+            const big = Math.ceil(-val / dig)    // 繰り下がり分
+            const small = val + dig * big
+            if (index < Decimal.INT) {
+                // 最上位桁を超過してなければ、big分を差し引き
+                this._intValue[index + 1] -= big;
+                this._intValue[index] = small;
+            } else {
+                // 差し引きできなかったらerror
+                throw RangeError("This answer will be minus value!")
+            }
         }
-        this._intValue[index] = small;
     }
 
-    private Borrow(index : number) {
+    private Borrow(index : number): void {
         // 指定した_intValueについて小数点以下の値があれば実行
-        let dig = 100000000;
+        let dig = Math.pow(10, Decimal.DIGIT);
         let val = this._intValue[index];
         let big = Math.floor(val);
         let small = (val - big) * dig;
-        if (index - 1 >= 0) {
+        if (index > 0) {
+            // 最下位桁を下回った分はこの処理ができず切り捨てられる
             this._intValue[index - 1] += small;
         }
         this._intValue[index] = big;
     }
 
-    public add(other : string){
+    public add(other: Decimal): void {
         // 加算
-        let tmpInt : number[] = new Array(5);
-        this.parseInt();
-        for (let i = 0; i < 5; i++) {
-            this._intValue[i] += tmpInt[i];
-            this.MoveUp(i);
+        const b : number[] = other.toNumbers()
+        for (let i = 0; i <= Decimal.INT; i++) {
+            this._intValue[i] += b[i];
+            this.Carry(i);
         }
     }
 
-    public sub(other : string){
+    public sub(other: Decimal): void{
         // 減算
-        let tmpInt : number[] = new Array(5);
+        const b : number[] = other.toNumbers()
         this.parseInt();
         for (let i = 0; i < 5; i++) {
-            this._intValue[i] -= tmpInt[i];
-            this.CarryDown(i);
+            this._intValue[i] -= b[i];
+            this.Carry(i);
         }
     }
 
-    public mul(other : number){
+    public mul(b: number): void{
         // 数値との乗算(逆数を入れれば除算)
         for (let i = 0; i < 5; i++) {
-            this._intValue[i] *= other;
+            this._intValue[i] *= b;
         }
         for (let i = 4; i >= 0; i--) {
             this.Borrow(i);
         }       
         for (let i = 0; i < 5; i++) {
-            this.MoveUp(i);
+            this.Carry(i);
         }
+    }
+    
+    public isSmallerThan(other: Decimal): Boolean {
+        // 引数より小さければtrue
+        const a = new Decimal(this.toString())
+        try{
+            a.sub(other)
+        } catch (e) {
+            if (e instanceof RangeError) {
+                return true
+            }
+        }
+        return false
     }
 }
