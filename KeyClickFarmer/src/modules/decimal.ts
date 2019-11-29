52d3fde0da0de7b109c402e9a94822700cbee15b
kato-hiroto <kato-hiroto@ed.tmu.ac.jp>
2019-11-29 16:45:44 +0900

    テスト通過

--- KeyClickFarmer/src/modules/decimal.ts
+++ KeyClickFarmer/src/modules/decimal.ts
@@ -1,5 +1,5 @@
 // 整数部(DIGIT * INT)桁，小数部(DIGIT)桁の正の数（0含む）が正しく扱える数値型
-export default class Decimal {
+export default class Decimal implements Number {
 
     static readonly DIGIT = 8;
     static readonly INT = 4;     // _intValue[1] ~ _intValue[INT]が整数値の保存領域
@@ -10,26 +10,50 @@ export default class Decimal {
     constructor(val: number | string | Decimal) {
         this.value = val;
     }
+    
+    // 継承
+    toFixed(fractionDigits?: number | undefined): string {
+        return (this.value as number).toFixed(fractionDigits);
+    }
+    toExponential(fractionDigits?: number | undefined): string {
+        return (this.value as number).toExponential(fractionDigits);
+    }
+    toPrecision(precision?: number | undefined): string {
+        return (this.value as number).toPrecision(precision);
+    }
+    valueOf(): number {
+        return this.value as number;
+    }
+    toLocaleString(locales?: string | string[] | undefined, options?: Intl.NumberFormatOptions | undefined): string {
+        return (this.value as number).toLocaleString(locales, options);
+    }
+
+    // 値のgetter, setter
+    get value(): number | string | Decimal {
+        return this._intValue[1] + this._intValue[0] / Decimal.DIGIT10;
+    } 
 
     set value(val: number | string | Decimal) {
         let intStr = "";
         let decStr = "";
         if (typeof val === "number") {
             intStr = Math.floor(val).toString();
-            decStr = (val - Math.floor(val)).toString();
+            decStr = Math.floor((val - Math.floor(val)) * Decimal.DIGIT10).toString();
         } else if (typeof val === "string") {
-            intStr = val.substring(0, val.indexOf("."));
-            decStr = val.substring(val.indexOf(".") + 1);
+            const dotPos = val.indexOf(".") < 0 ? val.length : val.indexOf(".");
+            intStr = val.substring(0, dotPos);
+            decStr = val.substring(dotPos + 1);
         } else {
             const tmp = val.toString();
-            intStr = tmp.substring(0, tmp.indexOf("."));
-            decStr = tmp.substring(tmp.indexOf(".") + 1);
+            const dotPos = tmp.indexOf(".") < 0 ? tmp.length : tmp.indexOf(".");
+            intStr = tmp.substring(0, dotPos);
+            decStr = tmp.substring(dotPos + 1);
         }
         // 格納する
         decStr += "0".repeat(Decimal.DIGIT);
         this._intValue[0] = Number(decStr.substring(0, Decimal.DIGIT));
         for (let i = 1; i <= Decimal.INT; i++) {
-            const endStart = intStr.length - i * Decimal.DIGIT;
+            const endStart = intStr.length - (i - 1) * Decimal.DIGIT;
             this._intValue[i] = Number(intStr.substring(Math.max(0, endStart - Decimal.DIGIT), endStart));
         }
     }
@@ -40,17 +64,18 @@ export default class Decimal {
     
     public toString(decimalLength: number = Decimal.DIGIT) : string {
         // 値を出力する
-        const intStr = this._intValue.slice(1, 5).reverse().join("");
-        const decStr = this._intValue[0].toString().replace(/0+$/, "");
-        return intStr + "." + decStr.substring(0, decimalLength);
+        const intStr = this._intValue.slice(1, 5).map((v, _) => {
+            return "0".repeat(Decimal.DIGIT - v.toString().length) + v.toString();
+        }).reverse().join("").replace(/^0+/, "");
+        const decStr = ("." + this._intValue[0].toString()).substring(0, decimalLength + 1).replace(/\.?0*$/, "");
+        return intStr + decStr;
     }
 
     private Carry(index : number): void {
         // 上の桁への値伝播
         const val = this._intValue[index];
-        const dig = Decimal.DIGIT10;
-        const big = Math.floor(val / dig);
-        const keep = val - big * dig;   // val > big * dig なので引けば必ず正になる
+        const big = Math.floor(val / Decimal.DIGIT10);
+        const keep = val - big * Decimal.DIGIT10;   // val > big * dig なので引けば必ず正になる
         if (index >= Decimal.INT && big !== 0) {
             // 繰り上がりができなかったらerror
             throw RangeError("This answer will be occured overflow!");
@@ -63,9 +88,8 @@ export default class Decimal {
     private Borrow(index : number): void {
         // 下の桁への値伝播
         const val = this._intValue[index];
-        const dig = Decimal.DIGIT10;
         const keep = Math.floor(val);
-        const small =  Math.floor((val - keep) * dig);   // 小数点以下DIGIT桁からは切り捨て
+        const small =  Math.floor((val - keep) * Decimal.DIGIT10);   // 小数点以下DIGIT桁からは切り捨て
         if (index > 0) {
             // 最下位桁を下回った分はこの処理ができず切り捨てられる
             this._intValue[index - 1] += small;
@@ -77,8 +101,8 @@ export default class Decimal {
         if (destructive) {
             // 破壊的加算
             const b : number[] = new Decimal(affector).toNumbers();
-            this._intValue.map((_, i) => {
-                this._intValue[i] += b[i];
+            this._intValue.forEach((_, i, a) => {
+                a[i] += b[i];
                 this.Carry(i);
             });
             return this;
@@ -93,8 +117,8 @@ export default class Decimal {
         if (destructive) {
             // 破壊的減算
             const b : number[] = new Decimal(affector).toNumbers();
-            this._intValue.map((_, i) => {
-                this._intValue[i] -= b[i];
+            this._intValue.forEach((_, i, a) => {
+                a[i] -= b[i];
                 this.Carry(i);
             });
             return this;
@@ -108,13 +132,14 @@ export default class Decimal {
     public mul(affector: number, destructive: Boolean = false): Decimal{
         if (destructive) {
             // 破壊的 : 数値との乗算
-            const b : number[] = new Decimal(affector).toNumbers();
-            const bVal = b[1] + b[0] / Decimal.DIGIT10;
-            this._intValue.map((_, i) => {
-                this._intValue[Decimal.INT - i] *= bVal;    // 逆順
+            const b : number = new Decimal(affector).value as number;
+            this._intValue.forEach((_, i, a) => {
+                a[i] *= b;
+            });
+            this._intValue.forEach((_, i) => {
                 this.Borrow(Decimal.INT - i);
             });
-            this._intValue.map((_, i) => {
+            this._intValue.forEach((_, i) => {
                 this.Carry(i);
             });
             return this;
@@ -138,7 +163,7 @@ export default class Decimal {
         } else {
             // 非破壊的
             const decimal = new Decimal(this);
-            return decimal.mul(n, true);
+            return decimal.divByPow10(n, true);
         }
     }
     
